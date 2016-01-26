dac1cfd96d596d288b359e2477a2d0a7e599733c
Avrumi Friedman <info@lakewoodprogramming.com>
2016-01-25 22:23:33 -0500

    Initial commit

---
+++ MvcApplication9/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int num = 0;
+            if (Request.Cookies["number"] != null)
+            {
+                num = int.Parse(Request.Cookies["number"].Value);
+            }
+
+            var cookie = new HttpCookie("number", (num + 1).ToString());
+            Response.Cookies.Add(cookie);
+
+            return View(num);
+        }
+
+    }
+}
---
+++ MvcApplication9/Scripts/index.js
@@ -0,0 +1,13 @@
+﻿$(function() {
+    $("#override").on('click', function() {
+        var number = $("#override-num").val();
+        $.cookie('number', number);
+        window.location.reload();
+    });
+
+    $("#delete").on('click', function() {
+        $.removeCookie('number');
+        window.location.reload();
+    });
+
+});
